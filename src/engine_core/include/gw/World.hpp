13b8751f064ab577c68e01f1ee485faabfdbdee3
Saitamadupauvre <lembrez.alban@gmail.com>
2026-09-14 11:32:42 +0200

    feat(engine_core): add World introspection APIs for editor reflection

--- src/engine_core/include/gw/World.hpp
+++ src/engine_core/include/gw/World.hpp
@@ -3,6 +3,8 @@
 #include <cstring>
 #include <typeindex>
 #include <unordered_map>
+#include <unordered_set>
+#include <vector>
 #include "gw/ComponentPool.hpp"
 #include "gw/Entity.hpp"
 
@@ -31,6 +33,45 @@ public:
     template <typename T>
     void removeComponent(Entity e) {
         poolFor<T>().remove(e);
+    }
+
+    template <typename T>
+    const std::vector<Entity>& each() const {
+        static const std::vector<Entity> kEmpty;
+        auto it = _pools.find(std::type_index(typeid(T)));
+        if (it == _pools.end()) return kEmpty;
+        return it->second.entities();
+    }
+
+    std::vector<std::type_index> componentTypesOf(Entity e) const {
+        std::vector<std::type_index> types;
+        for (const auto& [type, pool] : _pools) {
+            if (pool.get(e) != nullptr) types.push_back(type);
+        }
+        return types;
+    }
+
+    std::vector<Entity> allEntities() const {
+        std::vector<Entity> result;
+        std::unordered_set<uint32_t> seen;
+        for (const auto& [type, pool] : _pools) {
+            for (Entity e : pool.entities()) {
+                if (seen.insert(e.id).second) result.push_back(e);
+            }
+        }
+        return result;
+    }
+
+    void* getComponentRaw(Entity e, std::type_index type) {
+        auto it = _pools.find(type);
+        if (it == _pools.end()) return nullptr;
+        return it->second.get(e);
+    }
+
+    const void* getComponentRaw(Entity e, std::type_index type) const {
+        auto it = _pools.find(type);
+        if (it == _pools.end()) return nullptr;
+        return it->second.get(e);
     }
 
 private:
